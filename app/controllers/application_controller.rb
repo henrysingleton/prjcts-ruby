971c5f2895fc7762128e1a4db883be96fa28aac1
Henry Singleton <henry@limelightdigital.com>
2014-03-05 17:03:18 +1100

    Adding some comments and tidying up whitesapce

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  force_ssl
 
+# private method(s)
 private
 
-	def current_user
-		@current_user ||= User.find(session[:user_id]) if session[:user_id]
-		#caching in instance variable since this could be called many times during a request. 
-	end
-	
-	#this turns whatever method into a helper method that is then accessible from a view
-	
-	helper_method :current_user
+  # Current user method, used to check if user is logged in.
+  def current_user
+    
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # Note that we cache the instance variable in @current_user since this could be called many times during a request. 
+  end
+  
+  # This turns the passed method name into a helper method that is then accessible from a view, where controller methods would not otherwise be accessible.
+  helper_method :current_user
+  
+  # We can use teh authorize method to automatically redirect to the login page if we use it in some controllers. Note we haven't implemented this yet. 
+  def authorize
+    redirect_to login_url, alert: "Not authorized" if current_user.nil?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ActiveRecord::Base
-    has_secure_password
-
-   # attr_accessible :email, :password, :password_confirmation
-
-    validates_uniqueness_of :email
-
-    #Do not need to validate the password or password confirmation
+  has_secure_password
+  validates_uniqueness_of :email
 end
