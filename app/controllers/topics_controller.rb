5a06e27904143f2dea3e1df4838bb107736bf482
Henry Singleton <henry@limelightdigital.com>
2014-03-06 13:58:44 +1100

    Saving the topic works!

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,13 +1,21 @@
 class TopicsController < ApplicationController
   def new
-  	@topic = Topic.new
-  	@all_topics = Topic.select(:name)
+    @topic = Topic.new
+    @all_topics = Topic.select(:name)
   end
 
   def create
-  	@topic = Topic.new(params[:topic])
-  	@topic.save
-  	render "new"
+    @topic = Topic.new(topic_params)
+    @topic.save
+    render "new"
+  end
+
+  def topic_params
+    # Figured out that this is required in Rails 4. 
+    # It could also be written as 
+    #   `params[:topic].permit(:name, :upvotes, :downvotes)`
+    # But of course you would no longer require the :topic param element exists. 
+    params.require(:topic).permit(:name, :upvotes, :downvotes)
   end
 
   def vote
